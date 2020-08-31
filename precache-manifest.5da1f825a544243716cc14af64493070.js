cb80e56a988d627489e31d73890975eda1cea083
ramijraja5 <67172530+ramijraja5@users.noreply.github.com>
2020-08-31 13:43:58 +0200

    Add files via upload

---
+++ precache-manifest.5da1f825a544243716cc14af64493070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6792e84a0d4dd9aeb6793048e809b96d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54ce2f6aed192fe25cb7",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "54ce2f6aed192fe25cb7",
+    "url": "/static/js/2.12dc6d2f.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.12dc6d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7ca323c996a4d3e3d4",
+    "url": "/static/js/main.1a1a1723.chunk.js"
+  },
+  {
+    "revision": "d6ecf84704e8f0ba8d31",
+    "url": "/static/js/runtime-main.d97b9143.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/logo.6dbba458.svg"
+  }
+]);
